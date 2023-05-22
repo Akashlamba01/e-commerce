5c466fcd5c62197c77da602e7cec445ec2af0978
Rajan Lamba <rk1759593@gmail.com>
2023-05-22 12:56:13 +0530

    update

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ require("dotenv").config();
 require("./config/db");
 const { errors } = require("celebrate");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded());
 app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use("/", require("./router"));
 
